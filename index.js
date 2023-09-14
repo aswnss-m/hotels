b468542e7da5dd65612238d233b62b3ebadc3afa
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-09-15 00:11:34 +0530

    created logging route

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.get('/form',(req,res)=>{
 app.get("/details", (req, res) => {
     res.sendFile(path.join(__dirname+'/src/data/details.txt'));
 });
+app.post("/log",(res,req)=>{
+    console.log(req.body);
+    res.send("ok");
+})
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
     }
